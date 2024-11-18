8da9984c247a58c7b79eb243d08137dad99fd299
Nicolas Rocha Lacerda <nrochalacerda@gmail.com>
2024-11-18 14:04:13 -0300

    feat(gcloud): adicionado funcionalidade de upload para gcloud do relatorio
    feat(gcloud): adicionado funcionalidade de upload para gcloud do relatorio

--- ReportConsumer/Services/ReportService.cs
+++ ReportConsumer/Services/ReportService.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Storage.V1;
+using ReportConsumer.Entities;
 using ReportConsumer.Models;
 using ReportConsumer.Repositories;
 
@@ -10,6 +9,19 @@ namespace ReportConsumer.Services
 {
     public class ReportService
     {
+        private readonly StorageClient _storageClient;
+        private readonly string _bucketName = "bacen-reports";
+
+        public ReportService()
+        {
+            string jsonPath = @"C:\Users\nroch\OneDrive\Área de Trabalho\PROJETOS\ESTUDO\DOTNET\KEYS\org-hub-439600-30a266b2c435.json";
+
+            GoogleCredential credentials = GoogleCredential.FromFile(jsonPath);
+
+            _storageClient = StorageClient.Create(credentials);
+        }
+
+
         public void createReport(ReportModel report)
         {
             using (var context = new JudicialContext())
@@ -31,6 +43,8 @@ namespace ReportConsumer.Services
                         {
                             Console.WriteLine($"Processo: {processo.Numero}, Requerido: {processo.Requerido}");
                         }
+
+                        GenerateAndUploadCsv(processos);
                     }
                 }
                 catch (Exception ex)
@@ -40,6 +54,46 @@ namespace ReportConsumer.Services
 
 
 
+            }
+        }
+
+        public void GenerateAndUploadCsv(List<ProcessoJudicialEntity> processos)
+        {
+            try
+            {
+                var csvContent = new StringBuilder();
+                csvContent.AppendLine("Numero,Requerido,Advogado,ValorSolicitado"); 
+
+                foreach (var processo in processos)
+                {
+                    csvContent.AppendLine($"{processo.Numero},{processo.Requerido},{processo.Advogado},{processo.ValorSolicitado}");
+                }
+
+                string fileName = $"bacen-reports-{DateTime.Now.ToString("ddMMyy")}.csv";
+
+                UploadCsvToBucket(csvContent.ToString(), fileName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void UploadCsvToBucket(string csvContent, string fileName)
+        {
+            try
+            {
+                byte[] byteArray = Encoding.UTF8.GetBytes(csvContent);
+
+                using (var stream = new MemoryStream(byteArray))
+                {
+                    _storageClient.UploadObject(_bucketName, fileName, "text/csv", stream);
+                    Console.WriteLine($"File {fileName} uploaded successfully to {_bucketName}.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to upload file: {ex.Message}");
             }
         }
     }
